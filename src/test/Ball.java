7bd4038a59e5420508fcb656fb18e1020c2c54bf
KarimE92 <karimelbishouty@gmail.com>
2021-11-28 11:46:34 +0300

    Removed lines from RubberBall.java and added them to Ball.java with the intention of removing RubberBall.java due to redundancy

--- src/test/Ball.java
+++ src/test/Ball.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.RectangularShape;
 
@@ -25,7 +26,7 @@ abstract public class Ball {
     private float speedX;
     private float speedY;
 
-    public Ball(Point2D center,int radiusA,int radiusB,Color inner,Color border){
+    public Ball(Point2D center){
         this.center = center;
 
         up = new Point2D.Double();
@@ -33,24 +34,34 @@ abstract public class Ball {
         left = new Point2D.Double();
         right = new Point2D.Double();
 
-        up.setLocation(center.getX(),center.getY()-(radiusB / 2));
-        down.setLocation(center.getX(),center.getY()+(radiusB / 2));
+        final int radius = 10;
+        up.setLocation(center.getX(),center.getY()-(radius / 2));
+        down.setLocation(center.getX(),center.getY()+(radius / 2));
 
-        left.setLocation(center.getX()-(radiusA /2),center.getY());
-        right.setLocation(center.getX()+(radiusA /2),center.getY());
+        left.setLocation(center.getX()-(radius /2),center.getY());
+        right.setLocation(center.getX()+(radius /2),center.getY());
 
 
-        ballFace = makeBall(center,radiusA,radiusB);
+        ballFace = makeBall(center,radius,radius);
+
+        Color inner = new Color(255, 219, 88); //Defining the colour of the ball
+        Color border = inner.darker().darker(); //Defining the border of the ball
         this.border = border;
         this.inner  = inner;
         speedX = 0;
         speedY = 0;
     }
 
-    protected abstract Shape makeBall(Point2D center,int radiusA,int radiusB);
+    protected Shape makeBall(Point2D center, int radiusA, int radiusB) {
+
+        double x = center.getX() - (radiusA / 2);
+        double y = center.getY() - (radiusB / 2);
+
+        return new Ellipse2D.Double(x,y,radiusA,radiusB);
+    }
 
     public void move(){
-        float acceleration = (float) 0.0005; //this will make the ball get faster and faster every frame in the direction it is going
+        float acceleration = (float) 0.0025; //this will make the ball get faster and faster every frame in the direction it is going
         RectangularShape tmp = (RectangularShape) ballFace;
         if (speedX > 0 && speedX < 10){ //if the ball is moving to the right it should accelerate to the right up to a max speed
             speedX+=acceleration;
