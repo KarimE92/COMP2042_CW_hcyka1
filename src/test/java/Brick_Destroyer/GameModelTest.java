52f4f49783ffdc79e6932f2ee4f8076792f265dc
KarimE92 <karimelbishouty@gmail.com>
2021-12-13 15:06:25 +0300

    Added a @BeforeEach to create a new GameModel for every test

--- src/test/java/Brick_Destroyer/GameModelTest.java
+++ src/test/java/Brick_Destroyer/GameModelTest.java
@@ -1,67 +1,66 @@
 package Brick_Destroyer;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.awt.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameModelTest {
+    @BeforeEach
+    void CreateModel(){
 
+    }
+        private GameModel gameModel = new GameModel(new GameFrame());
 
     @Test
     void MiniBallTest(){
-        GameModel GameModel = new GameModel(new GameFrame());
-        GameModel.getLevels().nextLevel();
-        GameModel.getBall().moveTo((Point)GameModel.getLevels().getMultiballpowerup(0).getPosition());
-        GameModel.findImpacts();
-        assertEquals(3, GameModel.getMiniBalls().size());
+        gameModel.getLevels().nextLevel();
+        gameModel.getBall().moveTo((Point)gameModel.getLevels().getMultiballpowerup(0).getPosition());
+        gameModel.findImpacts();
+        assertEquals(3, gameModel.getMiniBalls().size());
     }
     @Test
     void ExtraLifeTest() {
-        GameModel GameModel = new GameModel(new GameFrame());
-        GameModel.getLevels().nextLevel(); //need to move to a level where there is 1 powerup
-        GameModel.getBall().moveTo((Point)GameModel.getLevels().getExtraLifepowerup(0).getPosition());
-        GameModel.findImpacts();
-        assertEquals(4, GameModel.getBallCount());
+        gameModel.getLevels().nextLevel(); //need to move to a level where there is 1 powerup
+        gameModel.getBall().moveTo((Point)gameModel.getLevels().getExtraLifepowerup(0).getPosition());
+        gameModel.findImpacts();
+        assertEquals(4, gameModel.getBallCount());
     }
     @Test
     void MiniBallCollectMultiBallPowerup(){
-        GameModel GameModel = new GameModel(new GameFrame());
-        GameModel.getLevels().nextLevel(); //need to get to a level where there are 2 powerups
-        GameModel.getLevels().nextLevel();
-        GameModel.getLevels().nextLevel();
-        GameModel.getBall().moveTo((Point)GameModel.getLevels().getMultiballpowerup(0).getPosition());
-        GameModel.findImpacts();
-        GameModel.getMiniBalls().get(0).moveTo((Point)GameModel.getLevels().getMultiballpowerup(1).getPosition());
-        GameModel.findImpacts();
-        assertEquals(6, GameModel.getMiniBalls().size());
+        gameModel.getLevels().nextLevel(); //need to get to a level where there are 2 powerups
+        gameModel.getLevels().nextLevel();
+        gameModel.getLevels().nextLevel();
+        gameModel.getBall().moveTo((Point)gameModel.getLevels().getMultiballpowerup(0).getPosition());
+        gameModel.findImpacts();
+        gameModel.getMiniBalls().get(0).moveTo((Point)gameModel.getLevels().getMultiballpowerup(1).getPosition());
+        gameModel.findImpacts();
+        assertEquals(6, gameModel.getMiniBalls().size());
     }
 
     @Test
     void MiniBallCollectExtraLifePowerup(){
-        GameModel GameModel = new GameModel(new GameFrame());
-        GameModel.getLevels().nextLevel(); //need to get to a level where there is 1 powerup
+        gameModel.getLevels().nextLevel(); //need to get to a level where there is 1 powerup
 
-        GameModel.getBall().moveTo((Point)GameModel.getLevels().getMultiballpowerup(0).getPosition());
-        GameModel.findImpacts();
-        GameModel.getMiniBalls().get(0).moveTo((Point)GameModel.getLevels().getExtraLifepowerup(0).getPosition());
-        GameModel.findImpacts();
-        assertEquals(4, GameModel.getBallCount());
+        gameModel.getBall().moveTo((Point)gameModel.getLevels().getMultiballpowerup(0).getPosition());
+        gameModel.findImpacts();
+        gameModel.getMiniBalls().get(0).moveTo((Point)gameModel.getLevels().getExtraLifepowerup(0).getPosition());
+        gameModel.findImpacts();
+        assertEquals(4, gameModel.getBallCount());
     }
     @Test
     void BrickCollisionTest(){
-        GameModel GameModel = new GameModel(new GameFrame());
-        GameModel.getBall().moveTo((new Point(50,50)));
-        GameModel.move();
-        GameModel.findImpacts();
-        assertEquals(29, GameModel.getLevels().getBrickCount());
+        gameModel.getBall().moveTo((new Point(50,50)));
+        gameModel.move();
+        gameModel.findImpacts();
+        assertEquals(29, gameModel.getLevels().getBrickCount());
     }
 
     @Test
     void BallAccelerationTest(){
-        GameModel GameModel = new GameModel(new GameFrame());
-        GameModel.getBall().setXSpeed(3);
-        GameModel.getBall().accelerate();
-        assertEquals(3.0025, GameModel.getBall().getSpeedX(),0.00001);
+        gameModel.getBall().setXSpeed(3);
+        gameModel.getBall().accelerate();
+        assertEquals(3.0025, gameModel.getBall().getSpeedX(),0.00001);
     }
 }
